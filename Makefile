820bd6b2d334088238885fa1f46782da471435f8
Jim Lloyd <jim.lloyd@gmail.com>
2014-10-19 18:09:40 -0700

    Unit tests for rough classification and vanilla parsing.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 default: lint test
 
 lint:
-	find lib test -name "*.js" | xargs node_modules/jshint/bin/jshint
+	find lib test -name "*.js" | xargs node_modules/jshint/bin/jshint --verbose
 
 test: lint
 	node_modules/mocha/bin/mocha --reporter=spec --ui tdd
--- test/test.js
+++ test/test.js
@@ -4,17 +4,66 @@
 
 require('./support/config');
 
+/* jshint -W030 */
+
 var Siteswap = require('../lib/siteswap').Siteswap;
 
 describe('Siteswap', function() {
 
   it('should initialize', function() {
-    var ss531 = new Siteswap('531');
+    new Siteswap('531');
   });
 
   it('should create valid throws array for vanilla siteswap', function() {
-    var ss531 = new Siteswap('531');
-    expect(ss531.getThrows()).to.be.a('array');
-    expect(ss531.getThrows()).to.deep.equal([5,3,1]);
+    var ss = new Siteswap('531');
+    expect(ss.kind.vanilla).to.be.true;
+    expect(ss.getThrows()).to.be.a('array');
+    expect(ss.getThrows()).to.deep.equal([5,3,1]);
   });
+
+  it('should not (yet) create throws array for multiplex', function() {
+    var ss = new Siteswap('[43]333');
+    expect(ss.getThrows()).to.be.undefined;
+    expect(ss.kind).to.deep.equal({
+      vanilla:false,
+      multiplex:true,
+      sync:false,
+      syncMulti:false
+    });
+  });
+
+  it('should not (yet) create throws array for sync', function() {
+    var ss = new Siteswap('(4,2x)*');
+    expect(ss.getThrows()).to.be.undefined;
+    expect(ss.kind).to.deep.equal({
+      vanilla:false,
+      multiplex:false,
+      sync:true,
+      syncMulti:false
+    });
+  });
+
+  it('should not (yet) create throws array for multisync', function() {
+    var ss = new Siteswap('(4,[42x])*');
+    expect(ss.getThrows()).to.be.undefined;
+    expect(ss.kind).to.deep.equal({
+      vanilla:false,
+      multiplex:false,
+      sync:false,
+      syncMulti:true
+    });
+  });
+
+  it('isSupported for multiplex should throw error', function() {
+    function makeMultiplex() {
+      var ss = new Siteswap('[43]333');
+      return ss.isSupported();
+    }
+    expect(makeMultiplex).to.throw(Error, /Multiplex siteswap not yet supported/);
+  });
+
+
 });
+
+
+
